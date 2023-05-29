c56b2d0967b0209ee8630cacda8243057d226245
Tyler Poon <tylerbill99@hotmail.com>
2023-05-28 20:44:56 -0400

    update crawler endpoint for more verbose errors

--- src/routes/CrawlTarget/create.ts
+++ src/routes/CrawlTarget/create.ts
@@ -1,3 +1,4 @@
+import { ValidationError } from 'yup'
 import { CrawlTarget } from '../../models/CrawlTarget'
 import { Request, Response, NextFunction } from 'express'
 
@@ -11,8 +12,11 @@ const createCrawlTarget = async (req: Request, res: Response, next: NextFunction
   } catch (err: any) {
     if (err.name === 'ValidationError') {
       // Yup errors from validate
-      const errors = err.errors.map((e: string) => ({ message: e }));
+      const errors = (err as ValidationError).inner.map((e) => ({ type: e.type, path: e.path, message: e.message }));
       return res.status(400).json({ errors });
+    } else if (err.code === '23505' && err.constraint === 'crawl_target_name_key') {
+      // Invalid key error from SQL
+      return res.status(409).json({ type: 'duplicate_key', path: 'name', message: err.message })
     }
 
     next(err)

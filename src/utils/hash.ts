a2bac2b1cd55171031c6ef78331efa4d9aecaa5e
Tyler Poon <tylerbill99@hotmail.com>
2023-07-14 23:52:22 -0400

    further testing

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,11 +1,11 @@
 import * as argon2 from "argon2"
 
 const hash = async (value: string): Promise<string> => {
-  return argon2.hash(value, {memoryCost: 8192, parallelism: 2})
+  return argon2.hash(value, {memoryCost: 4096, parallelism: 2, timeCost: 1})
 }
 
 const verify = async (valueHash: string, value: string): Promise<boolean> => {
-  return argon2.verify(valueHash, value, {memoryCost: 8192, parallelism: 2})
+  return argon2.verify(valueHash, value, {memoryCost: 4096, parallelism: 2, timeCost: 1})
 }
 
 export {

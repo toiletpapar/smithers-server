ba09f9f62683c7f68220f3d39bf2206a9a0278c0
Tyler Poon <tylerbill99@hotmail.com>
2023-07-15 13:34:46 -0400

    add bottleneck to hashing to ensure memory constraint is respected

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,11 +1,14 @@
 import * as argon2 from "argon2"
+import Bottleneck from "bottleneck"
+
+const limiter = new Bottleneck({maxConcurrent: 1})
 
 const hash = async (value: string): Promise<string> => {
-  return argon2.hash(value, {memoryCost: 1024, parallelism: 1, timeCost: 2})
+  return limiter.schedule(() => argon2.hash(value, {memoryCost: 1024, parallelism: 1, timeCost: 2}))
 }
 
 const verify = async (valueHash: string, value: string): Promise<boolean> => {
-  return argon2.verify(valueHash, value, {memoryCost: 1024, parallelism: 1, timeCost: 2})
+  return limiter.schedule(() => argon2.verify(valueHash, value, {memoryCost: 1024, parallelism: 1, timeCost: 2}))
 }
 
 export {
